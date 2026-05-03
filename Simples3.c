13c822f27146ffe4d17e459fbf54d551beddee65
santi-estilla20 <os967577@gmail.com>
2026-05-03 13:51:36 -0600

    Update Simples3.c

--- Simples3.c
+++ Simples3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() 
+{
+    float  pesos;
+    float  dolares;
+   
+    dolares, pesos = 0;
+    
+   printf("Ingrese la cantidad de dolares:");
+   scanf ("%f", &dolares);
+   
+   pesos= dolares * 11.96;
+  
+   printf("Los dolares equivalen a %f pesos", pesos);
+   
+    return 0;
+}
