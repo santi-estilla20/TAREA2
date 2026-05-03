15c988bad5a64a4b70456cb1ad51ae306bf9850c
santi-estilla20 <os967577@gmail.com>
2026-05-03 15:43:45 -0600

    Create if3.c

---
+++ if3.c
@@ -0,0 +1,44 @@
+void main ()
+{
+    int tiempo;
+    float dist;
+    float distT;
+    float precio;
+    float precioT;
+    float descuento;
+    
+    tiempo = 0, dist = 0, distT = 0, precio = 0, precioT = 0, descuento = 0;
+    
+    printf("Ingrese los dias de estancia:");
+    scanf("%d", &tiempo);
+    
+    printf("Ingrese la distancia de ida en kilometros:");
+    scanf("%f", &dist);
+    
+    distT= dist * 2;
+    precio = distT * 0.23;
+    
+    if(tiempo>7)
+    {
+        if(distT>800)
+        {
+          descuento = precio * 0.3;
+          precioT = precio - descuento;
+          printf("El precio del ticker ida y vuelta es de: %.2f", precioT);
+           
+        }
+        else
+        {
+           precioT = precio;
+           printf("El precio del ticker ida y vuelta es de: %.2f", precioT);
+        }
+    }
+    else
+    {
+        precioT = precio;
+        printf("El precio del ticker ida y vuelta es de: %.2f", precioT);
+    }
+    
+}
+    
+    
