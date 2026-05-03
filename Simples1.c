2ca7ab4155fd768a9f5c8a6b036bd6cdfcecc688
santi-estilla20 <os967577@gmail.com>
2026-05-03 11:58:22 -0600

    Create Simples1.c

---
+++ Simples1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int  num1;
+    int  num2;
+    int suma;
+    int resta;
+    int mult;
+    
+    num1, num2, suma, resta, mult = 0;
+    
+   printf("Ingrese un numero entero:");
+   scanf ("%d", &num1);
+   printf("Ingrese otro numero entero:");
+   scanf ("%d", &num2);
+   
+   suma= num1 + num2;
+   resta= num1 - num2;
+   mult = num1 * num2;
+   
+   printf("La suma de los numeros es: %d \n", suma);
+   printf("La resta de los numeros es: %d \n", resta);
+   printf("La multiplicacion de los numeros es: %d \n", mult);
+  
+    return 0;
+}
