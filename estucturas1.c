db7cb049f8614d026cd8f83bb7469392dfbec221
santi-estilla20 <os967577@gmail.com>
2026-05-03 16:49:32 -0600

    Update estucturas1.c

--- estucturas1.c
+++ estucturas1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main ()
+{
+    int num;
+    int tabla;
+    
+    num = 0; tabla;
+    
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &num);
+    
+    for (int cont = 1; cont <= 10; cont++)
+    {
+       tabla = num * cont;
+       printf(" %d x %d = %d\n", cont, num, tabla );
+    }
+       
+  return 0;      
+}
