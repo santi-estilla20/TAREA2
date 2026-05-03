85150ebb0037387c20f09e0bbb6bdda896c33f73
santi-estilla20 <os967577@gmail.com>
2026-05-03 16:12:31 -0600

    Update if4.c

--- if4.c
+++ if4.c
@@ -4,8 +4,9 @@ int main ()
 {
     int num1;
     int num2;
+    int num;
     
-    num1 = 0, num2 = 0;
+    num1 = 0, num2 = 0, num= 0;
     
     printf("Ingrese un numero entero: ");
     scanf("%d", &num1);
@@ -13,13 +14,19 @@ int main ()
     printf("Ingrese otro numero entero: ");
     scanf("%d", &num2);
     
+    if (num1 < num2)
+    {
+        num = num1;
+        num1 = num2;
+        num2 = num;   
+    }
     if (num1 % num2 == 0)
     {
         printf("El  %d es divisor de %d", num1, num2);
     }
     else 
     {
-        printf("El %d no es dividor de %d", num1, num2); 
+        printf("El %d no es divisor de %d", num1, num2); 
     }
     
   return 0;      
@@ -27,5 +34,6 @@ int main ()
 
 
 
+    
     
     
